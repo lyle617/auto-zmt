8a725fc6017b283f6ecf9cb018f5723780476caf
lyle <lin_gq2014@163.com>
2024-09-15 11:34:29 +0800

    Revert "auto_coder_131_chat_action.yml_a889affe528114be7906514b2d2c3517"
    This reverts commit 431a0a51cb9b59f7c6b5b0f68247520a84e5ad68.

--- src/toutiao_crawler.py
+++ src/toutiao_crawler.py
@@ -161,11 +161,10 @@ def process_response(response, timestamp):
             unique_titles.add(article['title'])
             deduplicated_articles.append(article)
 
-    deduplicated_articles = [article for article in deduplicated_articles if article.get('has_video', '0') != '1']
     deduplicated_articles = sorted(deduplicated_articles, key=lambda x: int(x['like_count']), reverse=True)[:500]
 
     with open(top_articles_path, mode='w', newline='', encoding='utf-8') as top_file:
-        fieldnames = ['title', 'publish_time', 'like_count', 'comment_count', 'media_name', 'source', 'abstract', 'article_url', 'tag', 'is_yaowen', 'article_sub_type', 'has_video']
+        fieldnames = ['title', 'publish_time', 'like_count', 'comment_count', 'media_name', 'source', 'abstract', 'article_url', 'tag', 'is_yaowen', 'article_sub_type']
         writer = csv.DictWriter(top_file, fieldnames=fieldnames)
         writer.writeheader()
         for row in deduplicated_articles:
