c0cca7128841e02fe86f967eac632bc983c046f1
lyle <lin_gq2014@163.com>
2024-09-15 11:35:36 +0800

    auto_coder_131_chat_action.yml_ac208c7bcf593009d4dabbfb57d46c83

--- src/toutiao_crawler.py
+++ src/toutiao_crawler.py
@@ -161,6 +161,8 @@ def process_response(response, timestamp):
             unique_titles.add(article['title'])
             deduplicated_articles.append(article)
 
+    # Filter out articles with has_video=1
+    deduplicated_articles = [article for article in deduplicated_articles if article.get('has_video', '') != '1']
     deduplicated_articles = sorted(deduplicated_articles, key=lambda x: int(x['like_count']), reverse=True)[:500]
 
     with open(top_articles_path, mode='w', newline='', encoding='utf-8') as top_file:
