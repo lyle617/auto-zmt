14fc207ae74f0c0bbf03e4870488db800eebbf4a
lyle <lin_gq2014@163.com>
2024-09-28 16:45:08 +0800

    auto_coder_pre_121_chat_action.yml_7d2d40fb844d4f4eeab6fd2028dda1d7

--- src/zhihu_crawler.py
+++ src/zhihu_crawler.py
@@ -41,7 +41,7 @@ class zhihuCrawler:
         file_path = os.path.join(output_dir, f'{question_id}.csv')
         with open(file_path, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            writer.writerow(['Author ID', 'Author Nickname', 'Author Type', 'Author Headline', 'Vote-up Count', 'Thanks Count', 'Comment Count', 'Content'])
+            writer.writerow(['Author ID', 'Author Nickname', 'Author Type', 'Author Headline', 'Likes Count', 'Thanks Count', 'Comment Count', 'Content'])
             for answer in answers:
                 target = answer.get('target', {})
                 author = target.get('author', {})
@@ -92,9 +92,10 @@ class zhihuCrawler:
 
 if __name__ == "__main__":
     cookie = "_xsrf=y8SqpKJbgHYnf0fqO36v8ZrTNApYNFe9; _zap=1e1966a1-bd6b-4a18-ba2e-1b699074eb91; d_c0=ANAVibVE_xePTmSODk-7qa7b9DX_DnJn1hk=|1705047763; __snaker__id=QVQHFx5ySDwFekqR; q_c1=b85740d1757d4d74bf04e7e3f43cc5bc|1705369475000|1705369475000; q_c1=b85740d1757d4d74bf04e7e3f43cc5bc|1725965099000|1705369475000; HMACCOUNT=5C1E600B38DAB84E; z_c0=2|1:0|10:1727350649|4:z_c0|80:MS4xaHZTYUF3QUFBQUFtQUFBQVlBSlZUWDl2eldkOHpzdWgydHFkNE9vQ1pXaV8wV0dtQnUwVS1BPT0=|9916518dffc53e9b4f43ce13c2f34cd282e2e8d76137b0bc084fe5f5f667f15c; tst=h; SESSIONID=u5bnXP5jYBDxg6M87bbcoAFa1LQYckDPETuR5GuS7JG; JOID=Vl0cAEkiE3QcQRUOcCVUq-IllRxkVioccx9ENxB9UDxuJyVCRN2BiX1GGAh70M_pSHjo9V8bH2se48rr8FJ00e4=; osd=V1gVAUojFn0dQhQLeSRXqucslB9lUyMdcB5BPhF-UTlnJiZDQdSAinxDEQl40crgSXvp8FYaHGob6svo8Vd90O0=; Hm_lvt_98beee57fd2ef70ccdd5ca52b9740c49=1727349902; __zse_ck=003_bvEOmZWPtbsKPCsKp+qHe4OROj4vqRu4La/2w9mSga6PJmJtJC74yxShaUy9uWwZ7TPk9HvzYop47b7Qx7TuqMHOwwGM+QqRT6CN58JUsKe2; Hm_lpvt_98beee57fd2ef70ccdd5ca52b9740c49=1727436021; BEC=6ff32b60f55255af78892ba1e551063a"
+    questions_id = '668753879';
     crawler = zhihuCrawler(cookie=cookie)
-    answers = crawler.get_answers('668753879')
-    crawler.save_answers(answers, '668753879')
+    answers = crawler.get_answers(question_id=questions_id, max_pages=5, order='')
+    crawler.save_answers(answers, questions_id)
     # if answers:
     #     for answer in answers:
     #         print(json.dumps(answer, indent=2, ensure_ascii=False))
